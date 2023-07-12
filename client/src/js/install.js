42f3640d91b6787098210415a29ef0460feb581e
gtotaku-tg <124806960+gtotaku-tg@users.noreply.github.com>
2023-07-12 10:17:02 -0400

    install.js file is modified logic for installing PWAs is added

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,44 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// * Add an event handler to the `beforeinstallprompt` event
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+
+    console.log('👍', "event", event);
+    event.preventDefault();
+
+    // Store the triggered events
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
+  });
+
+// * Implement a click event handler on the `butInstall` element
+
+butInstall.addEventListener('click', async () => {
+  
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+   return;
+  }
+
+  // Show prompt
+  promptEvent.prompt();
+  
+  // Reset the deferred prompt variable, it can only be used once.
+  window.deferredPrompt = null;
+  
+  butInstall.classList.toggle('hidden', true);
+});
+
+// * Add an event handler for the `appinstalled` event
+
+window.addEventListener('appinstalled', (event) => {
+  // Clear prompt
+  window.deferredPrompt = null;
+  console.log('installed');
+}); 
